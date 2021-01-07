1b9aa7ed5582bef76f11e7ccc52b8ef91c68a8ca
Chik3r <ikervaraalarcon@gmail.com>
2021-01-07 00:34:02 -0300

    magikarp

--- UI/tConfigModMenu.cs
+++ UI/tConfigModMenu.cs
@@ -4,8 +4,10 @@ using Terraria;
 using Terraria.UI;
 using Terraria.ModLoader;
 using System.IO;
+using Gajatko.IniFiles;
 using ReLogic.Graphics;
 using Microsoft.Xna.Framework.Graphics;
+using SevenZip;
 
 namespace tConfigWrapper.UI {
 	public class TConfigModMenu : UIState {
@@ -23,6 +25,24 @@ namespace tConfigWrapper.UI {
 			files = Directory.GetFiles(tConfigWrapper.ModsPath);
 			for (int i = 0; i < files.Length; i++) {
 				spriteBatch.DrawString(Main.fontMouseText, files[i], new Vector2(50, 10 + (i * 20)), Color.Cyan);
+			}
+
+			using (MemoryStream stream = new MemoryStream())
+			{
+				//SevenZipBase.SetLibraryPath(tConfigWrapper.SevenDllPath);
+				//string tmpPath = Main.SavePath + "\\tConfigWrapper\\tmpFile.7z";
+				string tmpPath = Path.Combine(Main.SavePath, "tConfigWrapper", "tmpFile.zip");
+				File.Copy(files[0], tmpPath, true);
+				FileStream fileStream = File.Open(tmpPath, FileMode.Open);
+				using (SevenZipExtractor extractor = new SevenZipExtractor(fileStream))
+				{
+					MemoryStream configStream = new MemoryStream();
+					extractor.ExtractFile("Config.ini", configStream);
+					configStream.Position = 0L;
+					IniFileReader configReader = new IniFileReader(configStream);
+				}
+				fileStream.Dispose();
+				File.Delete(tmpPath);
 			}
 		}
 	}
