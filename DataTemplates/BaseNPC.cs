6a504849ae43bf6a39fd0ff7175e6a164872f539
pollen__ <41d4ntheboss@gmail.com>
2021-01-08 23:06:26 -0600

    Oops my husk of a start to NPC loading broke everything

--- DataTemplates/BaseNPC.cs
+++ DataTemplates/BaseNPC.cs
@@ -3,6 +3,7 @@
 namespace tConfigWrapper.DataTemplates {
 
 	public class BaseNPC : ModNPC {
-
+		public override string Texture => "tConfigWrapper/DataTemplates/MissingTexture";
+		public override bool CloneNewInstances => true;
 	}
 }
