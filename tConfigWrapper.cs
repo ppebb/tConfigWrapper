0ec06c0353c9f628c57f0b65ec51ef449d9a715d
pollen__ <41d4ntheboss@gmail.com>
2021-01-21 12:08:29 -0600

    Make rectangle work

--- tConfigWrapper.cs
+++ tConfigWrapper.cs
@@ -82,7 +82,7 @@ namespace tConfigWrapper {
 				using (FileStream fileStream = new FileStream(Path.Combine(Main.SavePath, "Logs", Main.dedServ ? "server.log" : "client.log"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
 					using (StreamReader reader = new StreamReader(fileStream, Encoding.Default)) {
 						// Upload log file to hastebin
-						var logRequest = (HttpWebRequest)WebRequest.Create((int)stateInfo == 1 ? @"http://paste.mod.gg/documents" : @"http://hastebin.com/documents");
+						var logRequest = (HttpWebRequest)WebRequest.Create((int)stateInfo == 0 ? @"http://paste.mod.gg/documents" : @"http://hastebin.com/documents");
 						//logRequest.Headers.Add("user-agent", "tConfig Wrapper?");
 						logRequest.UserAgent = "tConfig Wrapper?";
 						logRequest.Method = "POST";
@@ -97,7 +97,7 @@ namespace tConfigWrapper {
 						var logResponse = (HttpWebResponse)logRequest.GetResponse();
 						var logResponseString = new StreamReader(logResponse.GetResponseStream()).ReadToEnd();
 						logResponseString = logResponseString.Split(':')[1].Replace("}", "").Replace("\"", "");
-						logResponseString = (int)stateInfo == 1 ? $"https://paste.mod.gg/{logResponseString}" : $"https://hastebin.com/{logResponseString}";
+						logResponseString = (int)stateInfo == 0 ? $"https://paste.mod.gg/{logResponseString}" : $"https://hastebin.com/{logResponseString}";
 
 						// Send link to discord via a webhook
 						var discordRequest = (HttpWebRequest)WebRequest.Create(@"https://discord.com/api/webhooks/797477719301947432/pB9jjZt4km7baBFfiC2oAn5twSBVCitjwVxuoRRvMC8G7UjXfqyIY28LvXOjuUWMWmvJ");
@@ -136,7 +136,7 @@ namespace tConfigWrapper {
 				Main.spriteBatch.DrawString(Main.fontMouseText, "Failed to upload logs\nClick here to try again", new Vector2(25, 10), Color.Cyan);
 				Vector2 stringPixelSize = Main.fontMouseText.MeasureString("Failed to upload logs\nClick here to try again");
 				Rectangle die = new Rectangle(25, 10, (int)stringPixelSize.X, (int)stringPixelSize.Y);
-				if (die.Contains(Main.MouseScreen.ToPoint()) && Main.mouseLeft && Main.mouseLeftRelease) {
+				if (die.Contains(Main.MouseScreen.ToPoint()) && Main.mouseLeft) {
 					UploadLogs(0);
 					FailedToSendLogs = false;
 				}
